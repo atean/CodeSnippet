77808ad7ce90029cf31021230ce2e13f4e7fa52e
Rahul Dhanokar <rr.dhanokar@gmail.com>
2017-05-17 17:29:40 +0530

    function memoizer implemented as decorator

---
+++ Memoizer_docorator.py
@@ -0,0 +1,27 @@
+class Memoize(object):
+
+    def __init__(self):
+        self.res_dict = {}
+
+    def __call__(self, func):
+
+        def wrapper(*args, **kwargs):
+            hash_key = self._hash(args, kwargs)
+            if hash_key not in self.res_dict:
+                res = func(*args, **kwargs)
+                self.res_dict[hash_key] = res
+                return res
+
+            return self.res_dict[hash_key]
+
+        return wrapper
+
+    @staticmethod
+    def _hash(arg, kwarg):
+        return str(arg) + str(kwarg)
+
+# usage :-
+
+# @Memoize
+# def func():
+#   pass
